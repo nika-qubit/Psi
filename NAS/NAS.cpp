5609997a7dff1598a4c276c7588dd74658d282fd
Ning Kang <ningk@google.com>
2025-04-17 15:06:17 -0700

    Added impl for listing fstab devices

--- NAS/NAS.cpp
+++ NAS/NAS.cpp
@@ -1,14 +1,26 @@
 ﻿#include "NAS.h"
 
+#include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 
 namespace nika::nas {
 
 std::vector<std::string> NAS::ListMountedDevices() {
-  return {"test_a", "test_b"};
+  std::ifstream mounts_file("/etc/fstab");
+  
+  if (!mounts_file.is_open()) {
+    std::cerr << "Error opening." << std::endl;
+  }
+  std::string line;
+  std::vector<std::string> mounted_devices;
+  while (std::getline(mounts_file, line)) {
+    mounted_devices.push_back(line);
+  }
+  return mounted_devices;
 }
-
+                                        jajaah
 
 }  // namespace nika::nas
 
