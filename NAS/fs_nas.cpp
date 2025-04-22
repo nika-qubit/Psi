e10fc3c2e007857653ca361248bc853b885dc321
Ning Kang <ningk@google.com>
2025-04-22 16:22:28 -0700

    Added basic fs based nas impl with relocated third_party dir for absl libraries.

--- NAS/fs_nas.cpp
+++ NAS/fs_nas.cpp
@@ -1,9 +1,30 @@
 #include "fs_nas.h"
 
+#include <filesystem>
+
+#include "absl/strings/string_view.h"
+#include "absl/log/log.h"
+
 namespace nika::nas {
 
+namespace {
+
+namespace fs = std::filesystem;
+
+constexpr absl::string_view kRootDir = "/media";
+
+}  // namespace
+
 std::vector<std::string> FsNAS::ListMountedDevices() const {
-  return {"ok"};
+  std::vector<std::string> devices;
+  for (const auto& entry : fs::directory_iterator(kRootDir)) {
+    if (fs::is_directory(entry)) {
+      devices.push_back(entry.path());
+    } else if (fs::is_regular_file(entry)) {
+      LOG(INFO) << "Skipping regular file: " << entry.path();
+    }
+  }
+  return devices;
 }
 
 }  // namespace nika::nas
--- NAS/labelled_fstab_nas.cpp
+++ NAS/labelled_fstab_nas.cpp
@@ -4,7 +4,6 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include "labelled_fstab_nas.h"
 
 namespace nika::nas {
 
